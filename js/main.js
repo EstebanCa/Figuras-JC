a84ae3870790bd3ead81525c4c5eeaf5b70e4f2f
EstebanCa <80774784+EstebanCa@users.noreply.github.com>
2021-03-16 10:57:31 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,53 @@
+function circuloFunction() {
+    var selector = document.getElementById("figura");
+    selector.className = '';
+    selector.classList.toggle("circulo");
+}
+
+function romboFunction() {
+    var selector = document.getElementById("figura");
+    selector.className = '';
+    selector.classList.toggle("rombo");
+}
+
+function rectanguloFunction() {
+    var selector = document.getElementById("figura");
+    selector.className = '';
+    selector.classList.toggle("rectangulo");
+}
+
+function topFunction() {
+    var selector = document.getElementById("figura");
+    selector.className = '';
+    selector.classList.toggle("top");
+}
+
+function rightFunction() {
+    var selector = document.getElementById("figura");
+    selector.className = '';
+    selector.classList.toggle("right");
+}
+
+function buttomFunction() {
+    var selector = document.getElementById("figura");
+    selector.className = '';
+    selector.classList.toggle("buttom");
+}
+
+function leftFunction() {
+    var selector = document.getElementById("figura");
+    selector.className = '';
+    selector.classList.toggle("left");
+}
+
+function dsdFunction() {
+    var selector = document.getElementById("figura");
+    selector.className = '';
+    selector.classList.toggle("dsd");
+}
+
+function dizFunction() {
+    var selector = document.getElementById("figura");
+    selector.className = '';
+    selector.classList.toggle("diz");
+}
